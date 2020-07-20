03c92cebf89e90724f2c43041344158f1dc97d1c
Ari <ari.huttunen@me.com>
2020-07-20 16:06:05 +0300

    rjstat - stats per date per municipality

--- thl/data-per-day-per-municipality.R
+++ thl/data-per-day-per-municipality.R
@@ -1,60 +1,50 @@
 library(tidyverse)
+library(rjstat)
+library(lubridate)
 
 base_url <- 'https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/'
-
-# try reading the data dimensions, and print them (not used)
-dimensions_url <- str_c(base_url, 'fact_epirapo_covid19case.dimensions.json')
-dimensions_text <- content(GET(dimensions_url), 'text')
-dimensions_text <- str_replace(dimensions_text, 'thl\\.pivot\\.loadDimensions\\(', '')
-dimensions_text <- str_replace(dimensions_text, '\\);', '')
-dimensions <- fromJSON(dimensions_text)
-dimensions <- as_tibble(dimensions)
-print(dimensions)
-municipalities <- as_tibble((dimensions[[3]][[1]])[[8]][[1]])
-print(municipalities)
-hus_municipalities <- as_tibble(municipalities[[8]][[21]])
-print(hus_municipalities)
-# ^ not used
 
 #
 # Case count per age group
 # 
 
-aineisto <- fromJSON(str_c(base_url, 
-                           'fact_epirapo_covid19case.json?column=ttr10yage-444309'))
+data <- fromJSONstat(str_c(base_url, 
+                          'fact_epirapo_covid19case.json?column=ttr10yage-444309'))
+count_per_age_group <- 
+  data$`Tartuntatautirekisterin COVID-19-tapaukset` %>%
+  as_tibble() %>%
+  rename(age=ttr10yage, case_count=value) %>%
+  mutate(case_count = as.numeric(case_count))
+colnames(count_per_age_group)
 
-#puretaan kategoriat paloiksi
-label <- as.data.frame(unlist(aineisto$dataset$dimension$ttr10yage$category$label))
-index <- as.data.frame(unlist(aineisto$dataset$dimension$ttr10yage$category$index))
-
-#Nimetään palaset
-names(label)<-"label"
-names(index)<-"index"
-
-#Laitetana vielä rivinumerot sarakkeiksi, jotta nämä saadaan yhteen.
-label<-rownames_to_column(label)
-index<-rownames_to_column(index)
-
-#Yhdistetään rivinimeä käyttäen
-kategoriat <- index %>% left_join(label,by="rowname")
-
-#otetaan data
-data <- as.data.frame(unlist(aineisto$dataset$value))
-
-#Nimetään
-names(data)<-"case_count"
-data<-rownames_to_column(data)
-data$rowname<-as.numeric(data$rowname)
-
-#Yhdistetään muuhun aineistoon
-dataset <- kategoriat %>% left_join(data,by=c("index"="rowname"))
-dataset <- as_tibble(dataset)
-dataset$case_count = as.numeric(dataset$case_count)
-View(dataset)
-dataset %>%
-  filter(label != 'Kaikki ikäryhmät') %>%
-  ggplot(aes(x=label, y=case_count)) +
+count_per_age_group %>%
+  filter(age != 'Kaikki ikäryhmät') %>%
+  ggplot(aes(x=age, y=case_count)) +
   geom_col() +
   theme_light()
 
+#
+# Case count per date per municipality
+# 
 
+data2 <- fromJSONstat(str_c(base_url, 
+                          'fact_epirapo_covid19case.json?',
+                          'row=dateweek2020010120201231-443702L&column=hcdmunicipality2020-445222'))
+count_per_date_per_mun <-
+  data2$`Tartuntatautirekisterin COVID-19-tapaukset` %>%
+  as_tibble() %>%
+  rename(date=dateweek2020010120201231, 
+         municipality=hcdmunicipality2020, 
+         case_count=value) %>%
+  mutate(date = as.Date(date), case_count = as.numeric(case_count)) %>%
+  filter(municipality != "Kaikki Alueet")
+
+colnames(count_per_date_per_mun)
+
+count_per_date_per_mun %>%
+  filter(date>'2020-06-01',
+         date<today()) %>%
+ggplot(aes(x=date, y=case_count, group=municipality)) +
+  geom_col() +
+  theme_light() +
+  facet_wrap(~ municipality)
