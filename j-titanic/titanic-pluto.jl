e340c5e4740fdc37f1d4fcbd7d7f6c857e8b0b10
Ari <ari.huttunen@me.com>
2020-10-19 12:09:39 +0300

    wider pluto

--- j-titanic/titanic-pluto.jl
+++ j-titanic/titanic-pluto.jl
@@ -13,6 +13,15 @@ begin
 	using BenchmarkTools
 end
 
+# ╔═╡ a612e268-11e9-11eb-2305-0175760c625d
+HTML("""
+		<style>
+			main {
+				max-width: 90%;
+			}
+		</style>
+	""")
+
 # ╔═╡ e401509a-11de-11eb-2139-f90fc50ae6cf
 begin
 	datadirectory = "../data/titanic/"
@@ -20,6 +29,11 @@ begin
 	describe(train_df)
 end
 
+# ╔═╡ a6b3a7a4-11ea-11eb-0d7c-0797092fec7e
+ENV["COLUMNS"] = 10
+
 # ╔═╡ Cell order:
+# ╠═a612e268-11e9-11eb-2305-0175760c625d
 # ╠═d5257f82-11de-11eb-0176-b1858e7894ae
 # ╠═e401509a-11de-11eb-2139-f90fc50ae6cf
+# ╠═a6b3a7a4-11ea-11eb-0d7c-0797092fec7e
